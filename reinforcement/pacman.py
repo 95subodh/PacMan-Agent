ddd285848eae50bf7eb74bedb3797079fd72c4a5
Subodh Yadav <subodh29031995@gmail.com>
2015-12-02 02:43:20 +0530

    2

--- reinforcement/pacman.py
+++ reinforcement/pacman.py
@@ -530,7 +530,7 @@ def readCommand( argv ):
     args = dict()
 
     # Fix the random seed
-    if options.fixRandomSeed: random.seed('PaCMaN')
+    if options.fixRandomSeed: random.seed('PacMan')
 
     # Choose a layout
     args['layout'] = layout.getLayout( options.layout )
